f308cfb84cc8bce335583235d815e954e561786b
OlivieFranklova <frankoli@fit.cvut.cz>
2024-10-09 15:44:50 +0200

    Update python main

--- smallfiles/main.py
+++ smallfiles/main.py
@@ -1,9 +1,14 @@
+import pandas as pd
+import numpy as np
+
 
 def main():
-    print("hey there")
+    df1 = pd.DataFrame(np.array([["Alice", 29], ["Bob", 31], ["Cathy", 22], ["David", 35]]),
+                       columns=['name', 'age'])
+    df2 = pd.DataFrame(np.array([["Alice", 40], ["Bob", 31], ["Catherine", 22], ["David", 35]]),
+                       columns=['name', 'age'])
+    common = df1.merge(df2,on=['name', 'age'])
+    print(df1[(~df1.name.isin(common.name))&(~df1.age.isin(common.age))])
 
-
-# Using the special variable
-# __name__
 if __name__ == "__main__":
     main()
