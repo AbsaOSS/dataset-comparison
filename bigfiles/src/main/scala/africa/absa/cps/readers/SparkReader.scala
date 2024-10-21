f3e56f7f84a8f145ccc4a0062fee699c49e18ed4
OlivieFranklova <frankoli@fit.cvut.cz>
2024-10-21 10:08:59 +0200

    Remove traits for reader and writer

--- bigfiles/src/main/scala/africa/absa/cps/readers/SparkReader.scala
+++ bigfiles/src/main/scala/africa/absa/cps/readers/SparkReader.scala
@@ -2,8 +2,14 @@ package africa.absa.cps.readers
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-object SparkReader extends Reader{
-
-  override def read(filePath: String, spark: SparkSession): DataFrame =
+object SparkReader{
+  /**
+   * Read data from a file
+   *
+   * @param filePath path to file
+   * @param spark SparkSession
+   * @return DataFrame
+   */
+  def read(filePath: String, spark: SparkSession): DataFrame =
     spark.read.parquet(filePath)
 }
--- bigfiles/src/main/scala/africa/absa/cps/writers/SparkWriter.scala
+++ bigfiles/src/main/scala/africa/absa/cps/writers/SparkWriter.scala
@@ -2,8 +2,15 @@ package africa.absa.cps.writers
 
 import org.apache.spark.sql.{DataFrame, SaveMode}
 
-object SparkWriter extends Writer {
-  override def write(filePath: String, data: DataFrame): Unit = {
+object SparkWriter{
+
+  /**
+   * Write the data to a file
+   *
+   * @param filePath path to file
+   * @param data DataFrame to write
+   */
+   def write(filePath: String, data: DataFrame): Unit = {
     println(s"Saving data ...")
     data.write.mode(SaveMode.Overwrite)
       .format("parquet")
