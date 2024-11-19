97c321ef40c32d1ece5cc7e968a81acc6fcf2239
OlivieFranklova <frankoli@fit.cvut.cz>
2024-11-19 12:34:27 +0100

    Move DiffComputeType to its own file

--- bigfiles/src/main/scala/africa/absa/cps/parser/Arguments.scala
+++ bigfiles/src/main/scala/africa/absa/cps/parser/Arguments.scala
@@ -1,13 +1,5 @@
 package africa.absa.cps.parser
 
-
-object DiffComputeType extends Enumeration {
-  val None, Row = Value
-}
-object DiffComputeTypeHelper {
-  implicit val diffComputeRead: scopt.Read[DiffComputeType.Value] = scopt.Read.reads(DiffComputeType.withName)
-}
-
 case class Arguments(
                    out: String = "",
                    inputA: String = "",
@@ -15,4 +7,3 @@ case class Arguments(
                    diff: DiffComputeType.Value = DiffComputeType.None,
                    exclude: Seq[String] = Seq()
                  )
-
