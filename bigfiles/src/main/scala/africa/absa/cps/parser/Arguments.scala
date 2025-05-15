fb5aa0394aa5b17306830dbf4037348fff5e3ddd
OlivieFranklova <frankoli@fit.cvut.cz>
2025-05-15 17:38:50 +0200

    Apply scalafmt formatting

--- bigfiles/src/main/scala/africa/absa/cps/parser/Arguments.scala
+++ bigfiles/src/main/scala/africa/absa/cps/parser/Arguments.scala
@@ -1,11 +1,10 @@
 package africa.absa.cps.parser
 
-
 case class Arguments(
-                      out: String = "",
-                      inputA: String = "",
-                      inputB: String = "",
-                      outFormat: OutputFormatType.Value = OutputFormatType.Parquet,
-                      diff: DiffComputeType.Value = DiffComputeType.None,
-                      exclude: Seq[String] = Seq()
+    out: String = "",
+    inputA: String = "",
+    inputB: String = "",
+    outFormat: OutputFormatType.Value = OutputFormatType.Parquet,
+    diff: DiffComputeType.Value = DiffComputeType.None,
+    exclude: Seq[String] = Seq()
 )
--- bigfiles/src/main/scala/africa/absa/cps/parser/OutputFormatType.scala
+++ bigfiles/src/main/scala/africa/absa/cps/parser/OutputFormatType.scala
@@ -3,7 +3,7 @@ package africa.absa.cps.parser
 object OutputFormatType extends Enumeration {
   type OutputFormatType = Value
   val Parquet: OutputFormatType = Value("parquet")
-  val CSV: OutputFormatType = Value("csv")
+  val CSV: OutputFormatType     = Value("csv")
 
   override def toString: String = super.toString
 }
